372c85de04e7e1639fc227e6290578b7d3ddd429
noro09 <takanoro09@gmail.com>
2017-06-30 17:34:18 +0900

    Update nvim_install.sh

--- nvim_install.sh
+++ nvim_install.sh
@@ -6,10 +6,10 @@ if [ $? -eq 0 ]; then
 else
 	echo "neovim をインストールします。"
 	yes |
-	sudo apt-get install software-properties-common python3-dev python3-pip
+	sudo apt-get -y install software-properties-common python3-dev python3-pip
 	sudo add-apt-repository ppa:neovim-ppa/unstable
-	sudo apt-get update
-	sudo apt-get install neovim
+	sudo apt-get -y update
+	sudo apt-get -y install neovim
 	sudo pip3 install -U pip
 	sudo pip3 install neovim
 	echo "neovim install complete!"
